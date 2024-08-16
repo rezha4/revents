63a3a55cdaf3c627b6e0d3bc515f2472920e3a74
rezha4 <rezha.bahari@gmail.com>
2024-08-16 13:09:33 +0700

    implemented semantic

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,14 @@
-function App() {
+import { Button } from "semantic-ui-react";
 
+function App() {
   return (
-    <h1>welcome to revents!</h1>
+    <>
+      <h1>welcome to revents!</h1>
+      <button className="ui icon red button">
+        <i className="user icon"></i> CSS Button
+      </button>
+      <Button icon="smile" content="React Button" color="green" />
+    </>
   );
 }
 
